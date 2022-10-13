47b613f170aed3832cf76699fa4e034d1013afbc
abfkh <84881348+abfkh@users.noreply.github.com>
2022-10-14 03:17:31 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+// function toggleMenu() {
+//     const menuToggle = document.querySelector('.menuToggle');
+
+
+//     menuToggle.classList.toggle('active');
+
+// };
+
+// const menuToggle = document.querySelector('.menuToggle');
+
+
+
+// navToggle.addEventListener('click', () => {
+//     nav.classList.toggle('menuToggle');
+// })
